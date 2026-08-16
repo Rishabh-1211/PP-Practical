10f868ea5a49f35541b24cbaae596ad315ee1c37
Sunita Yadav <sunita@email.com>
2026-08-16 22:04:23 +0530

    Lecture

---
+++ Practical 6/geometry/.ipynb_checkpoints/geometry-checkpoint.py
@@ -0,0 +1,9 @@
+import math
+
+def squareArea(edge):
+    # Calculate area of a square given edge length.
+    return edge * edge
+
+def circleArea(radius):
+    # Calculate area of a circle given radius
+    return math.pi * radius * radius
